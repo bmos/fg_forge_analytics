950d985e2b939b3c2808edadeeebaa3349f03eeb
Wil Thieme <wil.thieme@protonmail.com>
2024-02-29 18:53:46 -0500

    single window, split into 2 groups, cumulative

--- src/main.py
+++ src/main.py
@@ -14,33 +14,35 @@ def group_by_month(data: pd.DataFrame) -> pd.DataFrame:
     return data.groupby([data["Date"].dt.to_period("M"), "Item Name"])["Transaction #"].count().unstack("Item Name")
 
 
-def split_into_groups(n: int, unstacked_data) -> list:
-    sorted_items = unstacked_data.sum().sort_values(ascending=False).index.tolist()
-    group_size = len(sorted_items) // n
-    return [sorted_items[i * group_size: len(sorted_items) if i == n - 1 else (i + 1) * group_size] for i in range(n)]
+def split_into_groups(unstacked_data) -> list:
+    split_point = 1000
+    sorted_values = unstacked_data.sum().sort_values(ascending=False)
+    group1 = sorted_values[sorted_values > split_point].index.tolist()
+    group2 = sorted_values[sorted_values <= split_point].index.tolist()
+    return [group1, group2]
 
 
-def filter_and_plot(data: pd.DataFrame, items: list, i: int) -> None:
+def filter_and_plot(data: pd.DataFrame, items: list, i: int, ax: plt.Axes) -> None:
     items = [item for item in items if item in data.columns]
     subset_data = data.loc[:, items]
-    fig, ax = plt.subplots()
     for column in subset_data.columns:
         ax.plot(subset_data.index.to_timestamp(), subset_data[column].cumsum(), label=column)
     ax.xaxis.set_major_locator(mdates.MonthLocator())
     ax.xaxis.set_minor_locator(mdates.WeekdayLocator())
     ax.xaxis.set_major_formatter(mdates.DateFormatter("%b %Y"))
-    plt.title("Cumulative Sales over time (Chart {})".format(i + 1), fontsize=22)
-    plt.xlabel("Time", fontsize=16)
-    plt.ylabel("Cumulative number of sales / month", fontsize=16)
-    plt.legend(fontsize="large")
+    ax.set_title("Cumulative Sales over time (Chart {})".format(i + 1), fontsize=22)
+    ax.set_xlabel("Time", fontsize=16)
+    ax.set_ylabel("Cumulative number of sales / month", fontsize=16)
+    ax.legend(fontsize="large")
 
 
 def main():
     data = read_data(__file__)
     unstacked_data = group_by_month(data)
-    groups = split_into_groups(3, unstacked_data)
+    groups = split_into_groups(unstacked_data)
+    fig, axes = plt.subplots(len(groups), 1)
     for i, items in enumerate(groups):
-        filter_and_plot(unstacked_data, items, i)
+        filter_and_plot(unstacked_data, items, i, axes[i])
     plt.show()
 
 
